a5ce3ea87792e072e5df41fa61e35ca692e7bbb1
Nathan Swift <nswiftnswift@gmail.com>
2019-03-12 17:56:08 -0600

    ⏣ Switched Fcards to a Class Function

--- src/Fcards.js
+++ src/Fcards.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { Button, Image, Card, Icon } from 'semantic-ui-react'
 
-const Fcards = ({ fcard, key }) => (
-    <Card>
+
+class Fcards extends React.Component {
+    
+
+
+    render() {
+        return(
+        <Card>
         <Image src='https://banner2.kisspng.com/20180706/tp/kisspng-question-mark-computer-icons-check-mark-question-png-5b3fe4188f5878.9335032515309138165872.jpg' />
         <Card.Content>
         <Card.Header>Question</Card.Header> 
         </Card.Content>
         <Card.Description>
-            {  fcard.question }: { fcard.answer }
+        {  this.props.fcard.question }: { this.props.fcard.answer }
         </Card.Description>
-            <br/>
-            <Button>Press For Answer</Button>
+        <br/>
+        <Button>Press For Answer</Button>
         <Card.Content extra>
-            <a>
-                <Icon name='heart' />
-                Like everytime you get it right!
-            </a>
+        <a>
+        <Icon name='heart' />
+        Like everytime you get it right!
+        </a>
         </Card.Content>    
-    </Card>
-)
-
+        </Card>
+        )
+    }
+    }
+        
 export default Fcards
